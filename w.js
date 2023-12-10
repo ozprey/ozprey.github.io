61d89afed08ff040e941fe10ea6af523e8484f0d
ozprey <152182347+ozprey@users.noreply.github.com>
2023-12-10 23:04:55 +0530

    sw.js

--- w.js
+++ w.js
@@ -7,17 +7,38 @@ document.getElementById("toggle").addEventListener("click", function() {
 
   
   });
-    
+//
+window.onload = function() {
+  // Hide the preloader after 3 seconds (adjust the time as needed)
+  setTimeout(function() {
+    document.getElementById('plt').style.opacity=0;
+    document.getElementById('jxf').style.opacity=1;
 
+}, 3000);
+  console.log('hello');
+};
 
+//
+new Vivus('cannvas', { start: 'autostart', type: 'sync', duration: 100 });
+
+//
   let circle = gsap.timeline({
     scrollTrigger: {
       trigger: "#mnyy",
       start: "top center",
       end: "100px center",
-      scrub: 1,
+      scrub: 2,
       pin: "none",
-      markers: { startColor: "black", endColor: "black", fontSize: "18px", fontWeight: "bold", indent: 20 }
+      //markers: { startColor: "black", endColor: "black", fontSize: "18px", fontWeight: "bold", indent: 20 }
     }
   })
   circle.to("#mnyy", { scale: 40 });
+
+  ////SCROLL REVEAL
+ScrollReveal().reveal('.clb',{delay:0,origin      : 'bottom',
+distance    : '10px',
+duration    : 1000,
+opacity     : 0,
+easing      : 'linear',
+reset       : false
+});
